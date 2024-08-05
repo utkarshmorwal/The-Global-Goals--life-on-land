621755f1722fe836b959e54186628163817ae9eb
Utkarsh <thespidy1712@gmail.com>
2024-08-05 15:09:56 +0530

    n

--- images/about/about.js
+++
@@ -1,8 +0,0 @@
-const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
-const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
-const popoverButton = document.getElementById('popoverButton');
-const popover = document.getElementById('popover');
-
-// popoverButton.addEventListener('click', () => {
-//     popover.style.display = popover.style.display === 'block' ? 'none' : 'block';
-// });
